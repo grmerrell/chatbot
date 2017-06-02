8b7cc75b100b4a99e8cd78fc0eb4baaf3b4f4854
Eric Anderson <beatsforthemind@gmail.com>
2017-06-02 16:43:48 -0500

    Revert "broke synonyms into its own function and cleaned and organized"
    This reverts commit fddaa6bd3d22b842dab507d6cddc794d1b21587d.

--- allen_ginsborg_v_0.1.py
+++ allen_ginsborg_v_0.1.py
@@ -1,60 +1,16 @@
 import pronouncing
 import random
 import sys
-from nltk.corpus import wordnet as wn
-
-#################################################################
-# Common functions ##############################################
-#################################################################
 
 def rhymeSayer(content):
-    """
-		Takes user input and pulls off the last part of their line
-		and replaces it with a random rhyming word then returns 
-		what they said - the last word + a word that rhymes with the last word
-	"""
     line = content.rsplit(' ', 1)[0]
     word_to_rhyme = content.rsplit(' ', 1)[-1]
     rhyme_list = pronouncing.rhymes(word_to_rhyme)
     rhyme = random.choice(rhyme_list)
     return line + ' ' + rhyme
 
-def synonym(syn):
-	line = syn.rsplit(' ', 1)[0]
-	syn = syn.rsplit(' ', 1)[-1]
 
-	print syn
-
-	store = []
-	for synset in wn.synsets(syn):
-		for lemma in synset.lemmas():
-			store.append(lemma)
-	store
-
-	elements = []
-
-	for index, element in enumerate(store):
-		elements.append(element.name())
-
-    
-	stored_elements = []
-
-	for element in elements:
-		if element != elements[0]:
-			stored_elements.append(element)
-
-
-	rand_thes = random.choice(stored_elements)
-	return line + ' ' + rand_thes
-
-
-#################################################################
-# /Common functions #############################################
-#################################################################
-
-#################################################################
-# Slack integration #############################################
-#################################################################
+# Slack integration
 if len(sys.argv)>1: # If command line arguments are provided, attempt to connect to Slack. Otherwise, proceed via CLI
     import time
     from slackclient import SlackClient # Don't forget to pip install this
@@ -63,15 +19,15 @@ if len(sys.argv)>1: # If command line arguments are provided, attempt to connect
     AT_BOT = "<@" + BOT_ID + ">"
 
 def handle_command(command, channel):
-    """
+	"""
 		Receives commands directed at the bot and replies in rhyme.
 	"""
-    output_string = rhymeSayer(command)
-    slack_client.api_call(
-     "chat.postMessage",
-     channel=channel,
-     text=output_string,
-     as_user=True)
+	output_string = rhymeSayer(command)
+	slack_client.api_call(
+		"chat.postMessage",
+		channel=channel,
+		text=output_string,
+		as_user=True)
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -100,17 +56,17 @@ if __name__ == "__main__" and len(sys.argv) > 1:
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
 
-#################################################################
-# /Slack integration ############################################
-#################################################################
 
-#################################################################
-# Command line interface ########################################
-#################################################################
+# /Slack integration
 
 # this is where the users input goes in
 content = raw_input("""Allen ginsborg here ready to spit rhymes.  
 	What mad lyric should I spit this time?>>>""")
+
+# rhymeSayer function takes user input and pulls off the last part of
+# their line and replaces it with a random rhyming word"
+# then prints what they said - the last word + a word that rhymes with the last word
+
 
 content_with_rhymes = rhymeSayer(content)
 print content_with_rhymes
@@ -118,15 +74,79 @@ print content_with_rhymes
 # next, replace the end of a line with a synonym
 # then, not and an antonym
 
+from nltk.corpus import wordnet as wn
+
+# test
+wn.synsets("small")
+
+store = []
+# lemma is different than synonym sets
+# this will attach a bunch of synonyms
+# to the variable syn in array store
+def synonym(syn):
+    for synset in wn.synsets(syn):
+        for lemma in synset.lemmas():
+            store.append(lemma)
+    #n = len(store)
+
+syn = 'angry'
+
+#syn = raw_input("thesaurus time biotch!?>>>")
+
+synonym(syn)
+
+elements = []
+
+# then this will break down the lemmatized versions
+# of all of these words
+# and store the lemmatized synonyms in a list minus their part of speech
+# and repetitive index number
+for index, element in enumerate(store):
+    name = element.name()
+    elements.append(name)
+
+print(elements)
+
 print("Now we're going to have you enter a line and i'll replace the last \
 	word with a similar word that's not the same"
                                               )
 
 syn = raw_input("bro you want me to switch up your last word?>>>")
 
-synonymized_words = synonym(syn)
-print synonymized_words
+line = syn.rsplit(' ', 1)[0]
+print (line)
 
-#################################################################
-# /Command line interface #######################################
-#################################################################
+syn = syn.rsplit(' ', 1)[-1]
+print(syn)
+
+store = []
+
+def synonym(syn):
+    for synset in wn.synsets(syn):
+        for lemma in synset.lemmas():
+            store.append(lemma)
+    #n = len(store)
+
+synonym(syn)
+
+print ("checking stored object")
+store
+
+elements = []
+
+for index, element in enumerate(store):
+    name = element.name()
+    elements.append(name)
+
+stored_elements = []
+
+for element in elements:
+    if element != elements[0]:
+        stored_elements.append(element)
+
+
+def thesaurus_biotch(stored_elements):
+    rand_thes = random.choice(stored_elements)
+    print line + ' ' + rand_thes
+
+thesaurus_biotch(stored_elements)
